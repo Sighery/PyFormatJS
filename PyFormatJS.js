f392636a4a79819e9761321b1f672613dc881088
Sighery <sighery7@gmail.com>
2018-10-28 13:54:36 +0100

    Refactor script to ES6

--- PyFormatJS.js
+++ PyFormatJS.js
@@ -1,31 +1,53 @@
-String.prototype.pyformat = function() {
-	// Constants
-	var FIELD_TYPE_AUTOMATIC = 0;
-	var FIELD_TYPE_MANUAL = 1;
-	// Error messages
-	var MIXED_FIELDS_MSG = "Cannot switch between automatic field " +
-							"numbering and manual field specification";
-	var INVALID_ESCAPE_MSG = "Single '}' encountered in format string";
-	var MISSING_KEY_MSG = "Missing key '{}'";
-	var MISSING_ARG_MSG = "Missing argument for replacement field {}";
-	// Util functions
-	function checkIfArgExistsOrError(args, index) {
-		// Check if the given arg to replace for even exists or throw error
-		if (typeof args[index] === 'undefined') {
-			throw new IndexError(MISSING_ARG_MSG.pyformat(index));
-		}
+(function(){
+// Constants
+const FIELD_TYPE_AUTOMATIC = 0;
+const FIELD_TYPE_MANUAL = 1;
+// Error messages
+let MIXED_FIELDS_MSG = "Cannot switch between automatic field " +
+						"numbering and manual field specification";
+let INVALID_ESCAPE_MSG = "Single '}' encountered in format string";
+let MISSING_KEY_MSG = "Missing key '{}'";
+let MISSING_ARG_MSG = "Missing argument for replacement field {}";
+
+// Util functions
+function checkIfArgExistsOrError(args, index) {
+	// Check if the given arg to replace for even exists or throw error
+	if (typeof args[index] === 'undefined') {
+		throw new IndexError(MISSING_ARG_MSG.pyformat(index));
 	}
+}
 
+// Error classes for exceptions
+class ValueError extends Error {
+	constructor(msg) {
+		super(msg);
+		this.name = "ValueError";
+	}
+}
 
-	var args = Array.from(arguments);
-	var firstRun = true;
-	var fieldType;
+class KeyError extends Error {
+	constructor(msg) {
+		super(msg);
+		this.name = "KeyError";
+	}
+}
+
+class IndexError extends Error {
+	constructor(msg) {
+		super(msg);
+		this.name = "IndexError";
+	}
+}
+
+String.prototype.pyformat = function(...args) {
+	let firstRun = true;
+	let fieldType;
 	// In case of automatic field numbering, keep a count to know the next
 	// argument to use for the next replacement
-	var count = 0;
+	let count = 0;
 	// Since JavaScript has no named arguments like Python does, I'll implement
 	// that feature as the last argument being an object with those named args
-	var namedArgs = {};
+	let namedArgs = {};
 
 	if (args[args.length - 1] instanceof Object &&
 	args[args.length - 1].constructor === Object) {
@@ -35,7 +57,7 @@ String.prototype.pyformat = function() {
 
 
 	return this.replace(/(\{{1,}.*?\}{1,})/g, function(match, number) {
-		var strippedField = match.substr(1, match.length - 2);
+		let strippedField = match.substr(1, match.length - 2);
 
 		// If even stripped it still contains more {} inside, it means they
 		// want this one NOT replaced. {{}} used to escape.
@@ -74,7 +96,7 @@ String.prototype.pyformat = function() {
 		if (fieldType === FIELD_TYPE_MANUAL) {
 			// Check if it's a number field
 			if (/^\d+$/.test(strippedField) === true) {
-				var index = parseInt(strippedField, 10);
+				let index = parseInt(strippedField, 10);
 
 				checkIfArgExistsOrError(args, index);
 
@@ -91,26 +113,4 @@ String.prototype.pyformat = function() {
 		}
 	});
 };
-
-
-
-class ValueError extends Error {
-	constructor(msg) {
-		super(msg);
-		this.name = "ValueError";
-	}
-}
-
-class KeyError extends Error {
-	constructor(msg) {
-		super(msg);
-		this.name = "KeyError";
-	}
-}
-
-class IndexError extends Error {
-	constructor(msg) {
-		super(msg);
-		this.name = "IndexError";
-	}
-}
+})();
